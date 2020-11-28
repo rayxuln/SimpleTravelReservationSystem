847197154793bdb0df48086cb21b9676548c0f34
Rayxuln <imbosslx@gmail.com>
2020-11-28 20:15:07 +0800

    Refector windows, and get rid of the freaking UI designer

---
+++ src/main/java/com/raiix/travelreservationsystem/window/DataManagePanel.java
@@ -0,0 +1,159 @@
+package com.raiix.travelreservationsystem.window;
+
+import com.raiix.travelreservationsystem.App;
+import com.raiix.travelreservationsystem.model.*;
+
+import javax.swing.*;
+import javax.swing.event.TableModelEvent;
+import javax.swing.event.TableModelListener;
+import java.awt.event.*;
+
+public class DataManagePanel extends JPanel implements TableModelListener {
+
+    private JButton flightsButton;
+    private JButton busButton;
+    private JButton hotelsButton;
+    private JButton customersButton;
+
+    private JTable dataTable;
+    private JPopupMenu dataTableMenu;
+    private JMenuItem deleteMenuItem;
+    private JMenuItem addMenuItem;
+
+    private final BasicTableModel[] models;
+
+    private int currentDataManageTableIndex;
+
+    App app;
+
+    private void generateGUI(){
+        //数据管理
+        BoxLayout boxLayout = new BoxLayout(this, BoxLayout.Y_AXIS);
+        setLayout(boxLayout);
+
+        JToolBar toolBar = new JToolBar("切换数据表");
+        flightsButton = new JButton("航班");
+        busButton = new JButton("大巴");
+        hotelsButton = new JButton("宾馆");
+        customersButton = new JButton("客户");
+        toolBar.add(flightsButton);
+        toolBar.add(busButton);
+        toolBar.add(hotelsButton);
+        toolBar.add(customersButton);
+        toolBar.setFloatable(false);
+
+        dataTable = new JTable();
+        dataTable.setAutoCreateRowSorter(true);
+        dataTable.setDragEnabled(false);
+
+        JScrollPane scrollPane = new JScrollPane(dataTable);
+
+        add(toolBar);
+        add(scrollPane);
+    }
+
+    public DataManagePanel(App _app) {
+        setSize(800, 600);
+        app = _app;
+
+        generateGUI();
+
+        models = new BasicTableModel[]{app.flightTableModel, app.busTableModel, app.hotelsTableModel, app.customersTableModel};
+
+        currentDataManageTableIndex = 0;
+
+        flightsButton.addActionListener(makeDataManageButtonActionListener(0));
+        busButton.addActionListener(makeDataManageButtonActionListener(1));
+        hotelsButton.addActionListener(makeDataManageButtonActionListener(2));
+        customersButton.addActionListener(makeDataManageButtonActionListener(3));
+
+        dataTable.setModel(models[0]);
+        app.flightTableModel.addTableModelListener(this);
+        app.busTableModel.addTableModelListener(this);
+        app.hotelsTableModel.addTableModelListener(this);
+        app.customersTableModel.addTableModelListener(this);
+
+        dataTableMenu = new JPopupMenu("菜单");
+
+        deleteMenuItem = new JMenuItem("删除");
+        deleteMenuItem.addActionListener(new AbstractAction() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                int t = currentDataManageTableIndex;
+                BasicTableModel m = models[t];
+                m.remove((String) m.getValueAt(dataTable.getSelectedRow(), 0));
+            }
+        });
+        dataTableMenu.add(deleteMenuItem);
+
+        dataTableMenu.add(new JPopupMenu.Separator());
+
+        addMenuItem = new JMenuItem("添加");
+        addMenuItem.addActionListener(new AbstractAction() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                int t = currentDataManageTableIndex;
+                if (t == 0) {
+                    FlightTableModel m = (FlightTableModel) models[t];
+                    m.add(
+                            JOptionPane.showInputDialog(m.getColumnsName(0)),
+                            Double.parseDouble(JOptionPane.showInputDialog(m.getColumnsName(1))),
+                            Integer.parseInt(JOptionPane.showInputDialog(m.getColumnsName(2))),
+                            JOptionPane.showInputDialog(m.getColumnsName(4)),
+                            JOptionPane.showInputDialog(m.getColumnsName(5))
+                    );
+                } else if (t == 1) {
+                    BusTableModel m = (BusTableModel) models[t];
+                    m.add(
+                            JOptionPane.showInputDialog(m.getColumnsName(0)),
+                            Double.parseDouble(JOptionPane.showInputDialog(m.getColumnsName(1))),
+                            Integer.parseInt(JOptionPane.showInputDialog(m.getColumnsName(2)))
+                    );
+                } else if (t == 2) {
+                    HotelsTableModel m = (HotelsTableModel) models[t];
+                    m.add(
+                            JOptionPane.showInputDialog(m.getColumnsName(0)),
+                            Double.parseDouble(JOptionPane.showInputDialog(m.getColumnsName(1))),
+                            Integer.parseInt(JOptionPane.showInputDialog(m.getColumnsName(2)))
+                    );
+                } else if (t == 3) {
+                    CustomersTableModel m = (CustomersTableModel) models[t];
+                    m.add(
+                            JOptionPane.showInputDialog(m.getColumnsName(1))
+                    );
+                }
+            }
+        });
+        dataTableMenu.add(addMenuItem);
+
+
+        dataTable.addMouseListener( new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                if (e.getButton() == MouseEvent.BUTTON3) {
+                    deleteMenuItem.setEnabled(dataTable.getSelectedRowCount() > 0);
+                    dataTableMenu.show((JPanel) e.getSource(), e.getX(), e.getY());
+                }
+            }
+        });
+    }
+
+    public ActionListener makeDataManageButtonActionListener(final int id) {
+        return new AbstractAction() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                currentDataManageTableIndex = id;
+                dataTable.setModel(models[currentDataManageTableIndex]);
+            }
+        };
+    }
+
+    @Override
+    public void tableChanged(TableModelEvent e) {
+        if (e instanceof BasicTableModel.InvalidChangedKeyTableModelEvent) {
+            JOptionPane.showMessageDialog(dataTable, "该主键值与其他值冲突!", "错误", JOptionPane.ERROR_MESSAGE);
+        } else if (e instanceof BasicTableModel.InvalidAddKeyTableModelEvent) {
+            JOptionPane.showMessageDialog(dataTable, "该主键值与其他值冲突!", "错误", JOptionPane.ERROR_MESSAGE);
+        }
+    }
+}
--- src/main/java/com/raiix/travelreservationsystem/window/MainWindow.java
+++ src/main/java/com/raiix/travelreservationsystem/window/MainWindow.java
@@ -1,129 +1,40 @@
 package com.raiix.travelreservationsystem.window;
 
 import com.raiix.travelreservationsystem.App;
-import com.raiix.travelreservationsystem.model.*;
 
 import javax.swing.*;
-import javax.swing.border.TitledBorder;
-import javax.swing.event.ChangeEvent;
-import javax.swing.event.ChangeListener;
-import javax.swing.event.TableModelEvent;
-import javax.swing.event.TableModelListener;
-import javax.swing.table.*;
-import java.awt.*;
 import java.awt.event.*;
-import java.util.Vector;
 
-public class MainWindow extends JFrame implements WindowListener, TableModelListener {
+public class MainWindow extends JFrame implements WindowListener {
 
     JPanel mainPanel;
     private JTabbedPane tabbedPane;
-    private JButton flightsButton;
-    private JButton busButton;
-    private JButton hotelsButton;
-    private JButton customersButton;
-
-    private JTable dataTable;
-    private JPopupMenu dataTableMenu;
-    private JMenuItem deleteMenuItem;
-    private JMenuItem addMenuItem;
-
-    private final BasicTableModel[] models;
-
-    private int currentDataManageTableIndex;
 
     App app;
+
+    private void generateGUI(){
+        mainPanel = new JPanel();
+        BoxLayout boxLayout = new BoxLayout(mainPanel, BoxLayout.Y_AXIS);
+        mainPanel.setLayout(boxLayout);
+
+        tabbedPane = new JTabbedPane();
+        tabbedPane.addTab("数据管理", new DataManagePanel(app));
+        tabbedPane.addTab("预定", new JPanel());
+        tabbedPane.addTab("预定查询", new JPanel());
+
+        mainPanel.add(tabbedPane);
+        add(mainPanel);
+    }
 
     public MainWindow(App _app) {
         setTitle("简单旅行预定系统");
         setSize(800, 600);
         app = _app;
-
-        models = new BasicTableModel[]{app.flightTableModel, app.busTableModel, app.hotelsTableModel, app.customersTableModel};
-
-        currentDataManageTableIndex = 0;
+        generateGUI();
 
         addWindowListener(this);
         setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
 //        setExtendedState(getExtendedState() | JFrame.MAXIMIZED_BOTH);
-
-        add(mainPanel);
-
-        flightsButton.addActionListener(makeDataManageButtonActionListener(0));
-        busButton.addActionListener(makeDataManageButtonActionListener(1));
-        hotelsButton.addActionListener(makeDataManageButtonActionListener(2));
-        customersButton.addActionListener(makeDataManageButtonActionListener(3));
-
-        dataTable.setModel(models[0]);
-        app.flightTableModel.addTableModelListener(this);
-        app.busTableModel.addTableModelListener(this);
-        app.hotelsTableModel.addTableModelListener(this);
-        app.customersTableModel.addTableModelListener(this);
-
-        dataTableMenu = new JPopupMenu("菜单");
-
-        deleteMenuItem = new JMenuItem("删除");
-        deleteMenuItem.addActionListener(new AbstractAction() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                int t = currentDataManageTableIndex;
-                BasicTableModel m = models[t];
-                m.remove((String) m.getValueAt(dataTable.getSelectedRow(), 0));
-            }
-        });
-        dataTableMenu.add(deleteMenuItem);
-
-        dataTableMenu.add(new JPopupMenu.Separator());
-
-        addMenuItem = new JMenuItem("添加");
-        addMenuItem.addActionListener(new AbstractAction() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                int t = currentDataManageTableIndex;
-                if (t == 0) {
-                    FlightTableModel m = (FlightTableModel) models[t];
-                    m.add(
-                            JOptionPane.showInputDialog(m.getColumnsName(0)),
-                            Double.parseDouble(JOptionPane.showInputDialog(m.getColumnsName(1))),
-                            Integer.parseInt(JOptionPane.showInputDialog(m.getColumnsName(2))),
-                            JOptionPane.showInputDialog(m.getColumnsName(4)),
-                            JOptionPane.showInputDialog(m.getColumnsName(5))
-                    );
-                } else if (t == 1) {
-                    BusTableModel m = (BusTableModel) models[t];
-                    m.add(
-                            JOptionPane.showInputDialog(m.getColumnsName(0)),
-                            Double.parseDouble(JOptionPane.showInputDialog(m.getColumnsName(1))),
-                            Integer.parseInt(JOptionPane.showInputDialog(m.getColumnsName(2)))
-                    );
-                } else if (t == 2) {
-                    HotelsTableModel m = (HotelsTableModel) models[t];
-                    m.add(
-                            JOptionPane.showInputDialog(m.getColumnsName(0)),
-                            Double.parseDouble(JOptionPane.showInputDialog(m.getColumnsName(1))),
-                            Integer.parseInt(JOptionPane.showInputDialog(m.getColumnsName(2)))
-                    );
-                } else if (t == 3) {
-                    CustomersTableModel m = (CustomersTableModel) models[t];
-                    m.add(
-                            JOptionPane.showInputDialog(m.getColumnsName(1))
-                    );
-                }
-            }
-        });
-        dataTableMenu.add(addMenuItem);
-
-        MouseListener dataTableMouseListener = new MouseAdapter() {
-            @Override
-            public void mouseClicked(MouseEvent e) {
-                if (e.getButton() == MouseEvent.BUTTON3) {
-                    deleteMenuItem.setEnabled(dataTable.getSelectedRowCount() > 0);
-                    dataTableMenu.show(getMostRecentFocusOwner(), e.getX(), e.getY());
-                }
-            }
-        };
-
-        dataTable.addMouseListener(dataTableMouseListener);
     }
 
     public void windowOpened(WindowEvent e) {
@@ -159,92 +70,4 @@ public class MainWindow extends JFrame implements WindowListener, TableModelList
     public void windowDeactivated(WindowEvent e) {
 
     }
-
-    public ActionListener makeDataManageButtonActionListener(final int id) {
-        return new AbstractAction() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                currentDataManageTableIndex = id;
-                dataTable.setModel(models[currentDataManageTableIndex]);
-            }
-        };
-    }
-
-    @Override
-    public void tableChanged(TableModelEvent e) {
-        if (e instanceof BasicTableModel.InvalidChangedKeyTableModelEvent) {
-            JOptionPane.showMessageDialog(dataTable, "该主键值与其他值冲突!", "错误", JOptionPane.ERROR_MESSAGE);
-        } else if (e instanceof BasicTableModel.InvalidAddKeyTableModelEvent) {
-            JOptionPane.showMessageDialog(dataTable, "该主键值与其他值冲突!", "错误", JOptionPane.ERROR_MESSAGE);
-        }
-    }
-
-    {
-// GUI initializer generated by IntelliJ IDEA GUI Designer
-// >>> IMPORTANT!! <<<
-// DO NOT EDIT OR ADD ANY CODE HERE!
-        $$$setupUI$$$();
-    }
-
-    /**
-     * Method generated by IntelliJ IDEA GUI Designer
-     * >>> IMPORTANT!! <<<
-     * DO NOT edit this method OR call it in your code!
-     *
-     * @noinspection ALL
-     */
-    private void $$$setupUI$$$() {
-        mainPanel = new JPanel();
-        mainPanel.setLayout(new BorderLayout(0, 0));
-        tabbedPane = new JTabbedPane();
-        tabbedPane.setAutoscrolls(false);
-        tabbedPane.setEnabled(true);
-        tabbedPane.setMinimumSize(new Dimension(800, 600));
-        tabbedPane.setPreferredSize(new Dimension(800, 600));
-        tabbedPane.setTabLayoutPolicy(1);
-        tabbedPane.setTabPlacement(1);
-        mainPanel.add(tabbedPane, BorderLayout.CENTER);
-        final JPanel panel1 = new JPanel();
-        panel1.setLayout(new BorderLayout(0, 0));
-        tabbedPane.addTab("数据管理", panel1);
-        panel1.setBorder(BorderFactory.createTitledBorder(BorderFactory.createEmptyBorder(15, 15, 15, 15), null, TitledBorder.DEFAULT_JUSTIFICATION, TitledBorder.DEFAULT_POSITION, null, null));
-        final JToolBar toolBar1 = new JToolBar();
-        toolBar1.setBorderPainted(true);
-        toolBar1.setFloatable(false);
-        panel1.add(toolBar1, BorderLayout.NORTH);
-        toolBar1.setBorder(BorderFactory.createTitledBorder(BorderFactory.createEmptyBorder(15, 15, 15, 15), null, TitledBorder.DEFAULT_JUSTIFICATION, TitledBorder.DEFAULT_POSITION, null, null));
-        flightsButton = new JButton();
-        flightsButton.setText("航班");
-        toolBar1.add(flightsButton);
-        busButton = new JButton();
-        busButton.setText("大巴");
-        toolBar1.add(busButton);
-        hotelsButton = new JButton();
-        hotelsButton.setText("宾馆");
-        toolBar1.add(hotelsButton);
-        customersButton = new JButton();
-        customersButton.setText("客户");
-        toolBar1.add(customersButton);
-        final JScrollPane scrollPane1 = new JScrollPane();
-        panel1.add(scrollPane1, BorderLayout.CENTER);
-        scrollPane1.setBorder(BorderFactory.createTitledBorder(BorderFactory.createLineBorder(Color.black), "数据表", TitledBorder.DEFAULT_JUSTIFICATION, TitledBorder.DEFAULT_POSITION, null, null));
-        dataTable = new JTable();
-        dataTable.setAutoCreateRowSorter(true);
-        scrollPane1.setViewportView(dataTable);
-        final JPanel panel2 = new JPanel();
-        panel2.setLayout(new com.intellij.uiDesigner.core.GridLayoutManager(1, 1, new Insets(0, 0, 0, 0), -1, -1));
-        panel2.setName("");
-        tabbedPane.addTab("预定", panel2);
-        final JPanel panel3 = new JPanel();
-        panel3.setLayout(new com.intellij.uiDesigner.core.GridLayoutManager(1, 1, new Insets(0, 0, 0, 0), -1, -1));
-        tabbedPane.addTab("预定信息查询", panel3);
-    }
-
-    /**
-     * @noinspection ALL
-     */
-    public JComponent $$$getRootComponent$$$() {
-        return mainPanel;
-    }
 }
-
